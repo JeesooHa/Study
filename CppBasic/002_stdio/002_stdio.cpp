a12852934a1dd5fab5cd392e45bab709cec8f59d
Jeesoo Ha <jsha2913@gmail.com>
2022-06-02 23:16:07 +0900

    cpp 기초 코드 추가

---
+++ CppBasic/002_stdio/002_stdio.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <iomanip>
+
+int main()
+{
+	int n = 10;
+	std::cout << n << std::endl;					// 10진수
+	std::cout << std::hex << n << std::endl;		// 16진수
+	std::cout << n << std::endl;					// 16진수로 유지됨
+	std::cout << std::dec << n << std::endl;		// 10진수로 변경됨
+
+	std::cout << std::setw(10) << "hello" << std::endl;			// '  'hello
+	std::cout << std::setfill('#') << std::setw(10) <<
+		"hello" << std::endl;		// #####hello
+	std::cout << std::setfill('#') << std::left << std::setw(10) <<
+		"hello" << std::endl; //hello#####
+}
+
---
+++ CppBasic/003_variable/003_variable.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+struct Point {
+	int x = 0; // 맴버 초기화 C++11
+	int y = 0;
+};
+int main() {
+	struct Point p1; // C style
+	Point p2; // C++ style	
+
+	int n1 = 0b10;        // c++11
+	int n2 = 1'000'000;   // c++11
+
+	int n3 = 0;   // 함수 중간에 변수선언 가능
+
+	bool b = true;   // false C99
+}
