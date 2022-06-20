f7016a233cace1321457ab34dad13776425d3dfe
Jeesoo Ha <jsha2913@gmail.com>
2022-06-21 00:08:11 +0900

    타입 기타

---
+++ CppBasic/4_type/type_etc.cpp
@@ -0,0 +1,10 @@
+int main(){
+    bool b = true;      // false, 0, 1 - c++98
+    long long n = 10;   // c++11. 64bit 정수
+
+    int n1 = 0b1000'0000;     // 2진수 표기법 - c++11
+    int n2 = 1'000'000;     // digit separator - c++11
+
+    int* p1 = 0;
+    int* p2 = nullptr;      // c++11 부터
+}
