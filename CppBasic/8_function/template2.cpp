6c3953296c924576c70cfda15eb9cf9973916ddc
Jeesoo Ha <jsha2913@gmail.com>
2022-06-21 23:36:04 +0900

    template 함수 강좌 수강

---
+++ CppBasic/8_function/template2.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+
+template<class T>
+T square(T a)
+{
+    return a*a;
+}
+
+int main()
+{
+    square<int>(3);
+    square(3);  // square는 함수 아님
+                //=> square<int>(3)
+
+    //printf("%p\n", &square);    //error
+    printf("%p\n", &square<int>);       //ok
+    printf("%p\n", &square<double>);    //ok
+}
