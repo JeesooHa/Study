f365b594e220be198e094301843a4fb716f2bd3d
Jeesoo Ha <jsha2913@gmail.com>
2022-07-14 15:42:25 +0900

    상수맴버함수

---
+++ CppBasic/17_const_member_func/mutable.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+class Rect
+{
+    int x,y,w,h;
+    mutable int cnt = 0;
+public:
+    Rect(int x, int y, int w, int h)
+    : x{x}, y{y},w{w},h{h} {}
+
+   int getArea() const
+    {
+        ++cnt;
+        std::cout << cnt << std::endl;
+        return w*h;
+    }
+};
+
+int main()
+{
+    Rect r1(1,1,10,10);
+    Rect r2(1,1,10,10);
+
+    r1.getArea();
+    r2.getArea();
+    r2.getArea();
+}
